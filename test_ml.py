61255132dcb61bea87651e4036cebf65d9679332
Sarahew555 <sarahew555@gmail.com>
2025-08-08 14:38:47 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -2,6 +2,8 @@ import pytest
 from sklearn.svm import SVC
 from ml.model import train_model
 from ml.data import process_data
+from ml.model import compute_model_metrics
+from sklearn.metrics import fbeta_score, precision_score, recall_score
 
 # TODO: add necessary import
 
@@ -20,7 +22,18 @@ def test_model_metrics_expected_values():
     """
     Check that 
     """
-    
+    y_true = np.array([1, 1, 0, 1, 0, 0, 1])
+    y_pred = np.array([1, 0, 0, 1, 1, 0, 1])
+
+    expected_precision = precision_score(y_true, y_pred, zero_division=1)
+    expected_recall = recall_score(y_true, y_pred, zero_division=1)
+    expected_fbeta = fbeta_score(y_true, y_pred, zero_division=1)
+
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    assert np.isclose(precision, expected_precision, atol=1e-7)
+    assert np.isclose(recall, expected_recall, atol=1e-7)
+    assert np.isclose(fbeta, expected_fbeta, atol=1e-7)
     
 
 
