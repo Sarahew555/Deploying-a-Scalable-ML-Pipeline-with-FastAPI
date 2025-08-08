aa0adb837a630c8c02f74e618520e92629d37dca
Sarahew555 <sarahew555@gmail.com>
2025-08-08 14:14:12 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,14 +1,18 @@
 import pytest
+from sklearn.svm import SVC
+from ml.model import train_model
+from ml.data import process_data
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_model_returns_exp_algorithm():
     """
-    # add description for the first test
+    Test 1 verifies that the trained model object is the same as the expected algorithm type.
     """
-    # Your code here
-    pass
-
+    X, y = 
+    model = train_model(X, y)
+    assert isinstance(model, SVC)
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
