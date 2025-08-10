b5fe7893ef27feebab4694ba2b557a6b297031ef
Sarahew555 <sarahew555@gmail.com>
2025-08-10 17:06:01 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,6 +1,8 @@
 import pytest
-from ml.model import train_model, compute_model_metrics
+from ml.model import train_model, compute_model_metrics, inference
 import numpy as np
+import unittest
+from unittest import mock
 # TODO: add necessary 
 
 # TODO: implement the first test. Change the function name and input as needed
@@ -33,5 +35,10 @@ def test_three():
     """
     # add description for the third test
     """
-    # Your code here
-    pass
+    X_test = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]])
+    expected = np.array([0, 1, 0])
+    mock_model = mock.Mock()
+    mock_model.predict.return_value = expected
+    actual = inference(mock_model, X_test)
+    mock_model.predict.assert_called_once_with(X_test)
+    np.testing.assert_array_equal(actual, expected)
