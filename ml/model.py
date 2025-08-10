29c641841c2dd570fdab11d1dbeb56e58a4f2108
Sarahew555 <sarahew555@gmail.com>
2025-08-10 13:50:27 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -123,7 +123,7 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
-    data_slice = data[data[column_name] == [slice_value]
+    data_slice = data[data[column_name] == slice_value]
     
     X_slice, y_slice, _, _ = process_data(
         data=data_slice,
