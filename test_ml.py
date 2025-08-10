800b29ba2639a6cd293c534b87ca9f5a35e14ab9
Sarahew555 <sarahew555@gmail.com>
2025-08-10 15:11:58 -0500

    Update test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,16 @@
 import pytest
-# TODO: add necessary import
+from ml.model import train_model
+# TODO: add necessary 
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
     # add description for the first test
     """
-    # Your code here
-    pass
+    X_train = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    y_train = np.array([0, 1, 0, 1])
+    model = train_model(X_train, y_train)
+    assert isinstance(model, xgb.XGBClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
