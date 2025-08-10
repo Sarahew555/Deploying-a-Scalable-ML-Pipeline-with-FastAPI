a89d4ffc7edb916a20a5355441f4d2582d98ad55
Sarahew555 <sarahew555@gmail.com>
2025-08-10 13:53:11 -0500

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -20,7 +20,7 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-    model = xbg.XBGClassifier(random_state=4)
+    model = xgb.XBGClassifier(random_state=4)
     model.fit(X_train, y_train)
     return model
 
